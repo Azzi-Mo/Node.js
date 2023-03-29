6170b6a08cbc3397b06ecba27c7d8c1754765be3
Azzi-Mo <alhwarymostafa@gmail.com>
2023-03-29 21:23:16 +0200

    edit

--- views/app.js
+++ views/app.js
@@ -1,24 +1,48 @@
-const express = require("express");
-const http = require("http");
+const express = require('express');
 const app = express();
-const socketIO = require("socket.io");
-const ejs = require("ejs");
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
-app.set("view engine", "ejs");
+// Array of books
+const books = [
+  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+  { title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
+  { title: '1984', author: 'George Orwell', year: 1949 }
+];
 
-// Search route
-app.get('/search', (req, res) => {
-  const searchTerm = req.query.q.toLowerCase();
+// Set up middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-  const results = books.filter(book =>
-    book.title.toLowerCase().includes(searchTerm) ||
-    book.author.toLowerCase().includes(searchTerm) ||
-    book.year.toString().includes(searchTerm)
-  );
-
-  res.render('search', { searchTerm, results });
+// Render the book list page
+app.get('./books.ejs', (req, res) => {
+  res.render('./books.ejs', { books });
 });
 
+// // Render the new book form page
+// app.get('/books/new', (req, res) => {
+//   res.render('new-book');
+// });
+
+// // Add a new book to the array
+// app.post('./books.ejs', (req, res) => {
+//   const { title, author, year } = req.body;
+//   books.push({ title, author, year });
+//   res.redirect('./books.ejs');
+// });
+
+// // Search for a book by title, author, or year
+// app.post('/books/search', (req, res) => {
+//   const { searchTerm } = req.body;
+//   const results = books.filter(book =>
+//     book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+//     book.author.toLowerCase().includes(searchTerm.toLowerCase()) ||
+//     book.year.toString().includes(searchTerm)
+//   );
+//   res.render('search-results', { results, searchTerm });
+// });
+
+// Start the server
 app.listen(3000, () => {
-  console.log("server is runing..");
+  console.log('Server listening on port 3000');
 });
